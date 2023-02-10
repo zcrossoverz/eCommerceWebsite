806a1d211da7c3f11d6957b88c9d048409e7b8e7
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-02-10 11:08:09 +0700

    add error interface, user services, setup user controllers and user routes

--- server/app/services/user.service.ts
+++ server/app/services/user.service.ts
@@ -1,5 +1,7 @@
+import { DeleteResult, UpdateResult } from "typeorm";
 import { AppDataSource } from "../database";
 import { User } from "../entities/user.entity";
+import { BadRequestError, ErrorInterface } from "../utils/error";
 
 enum UserRole {
   ADMIN = "admin",
@@ -24,31 +26,67 @@ interface UserReturnInterface extends UserInterface {
 const userRepository = AppDataSource.getRepository(User);
 
 export const createUser = async ({
+  email,
+  password,
+  firstName,
+  lastName,
+  address,
+  phone,
+}: UserInterface): Promise<ErrorInterface | UserReturnInterface> => {
+  const emailExists = await userRepository.findOneBy({ email: email });
+  if (emailExists) return BadRequestError("email already exists!");
+
+  if (phone) {
+    const phoneExists = await userRepository.findOneBy({ phone: phone });
+    if (phoneExists) return BadRequestError("phone number already exists!");
+  }
+
+  const newUser = userRepository.create({
     email,
     password,
     firstName,
     lastName,
     address,
-    phone
-}: UserInterface): Promise<Error | UserReturnInterface> =>  {
-
-    const newUser = await userRepository.create({
-      email,
-      password,
-      firstName,
-      lastName,
-      address,
-      phone,
-    });
-    const create = await userRepository.save(newUser);
-    return create;
+    phone,
+  });
+  return await userRepository.save(newUser);
 };
 
-export const getOneUser = async (id: number): Promise<null | UserReturnInterface> => {
-    const result = userRepository.findOneBy({
-        id: id
-    });
+export const getOneUser = async (
+  id: number
+): Promise<ErrorInterface | UserReturnInterface> => {
+  const result = await userRepository.findOneBy({ id });
+  return result ? result : BadRequestError("user not found!");
+};
 
-    return result;
+export const updateOneUser = async (
+  id: number,
+  user: UserInterface
+): Promise<ErrorInterface | UpdateResult> => {
+  const findUser = await userRepository.findOneBy({ id });
+  if (!findUser) return BadRequestError("user not found!");
+
+  const { email, phone } = user;
+  if (email) {
+    const emailExists = await userRepository.findOneBy({ email });
+    if (emailExists) return BadRequestError("email already exists!");
+  }
+
+  if (phone) {
+    const phoneExists = await userRepository.findOneBy({ phone });
+    if (phoneExists) return BadRequestError("phone number already exists!");
+  }
+
+  return await userRepository.update({ id }, user);
+};
+
+export const deleteOneUser = async (id: number): Promise<ErrorInterface | DeleteResult> => {
+  const findUser = await userRepository.findOneBy({ id });
+  if (!findUser) return BadRequestError("user not found!");
+
+  return await userRepository.delete({ id });
+};
+
+export const getAllUser = async (): Promise<Array<UserReturnInterface>> => {
+  return await userRepository.find();
 }
-
