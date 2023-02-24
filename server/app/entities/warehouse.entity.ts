09659f2b263aa6cfcc1f87c7092bb4afe6b8c4ea
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-02-24 14:45:03 +0700

    definition warehouse entity

---
+++ server/app/entities/warehouse.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("warehouse")
+export class Warehouse {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    quantity!: number;
+}
