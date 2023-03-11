c5c10035e9d8a192307b3a59395ad81537931511
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-03-11 20:36:53 +0700

    mark as paid payment

--- server/app/routes/order.route.ts
+++ server/app/routes/order.route.ts
@@ -13,6 +13,7 @@ export const OrderRoutes = (app: Express) => {
     router.get(/^\/get_all(\?)?(((limit=[0-9])|(page=[0-9]))?(\%26)?){2}$/, order.getAllOrder);
     router.post("/select_payment_method/:order_id", payment.selectMethod);
     router.patch("/update_status_order/:order_id", order.updateStatusOrder);
+    router.patch("/update_status_payment/:order_id", payment.updateStatusPayment);
     router.delete("/:order_id", order.deleteOrder);
 
     app.use("/api/order", router);
