b865d1fce8d0155e465121f621dba69f8b708902
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-03-12 13:36:59 +0700

    fix path upload

--- server/app/middlewares/upload.ts
+++ server/app/middlewares/upload.ts
@@ -1,12 +1,11 @@
 import multer from "multer";
 import * as time from "../utils/time";
 import fs from "fs";
-import path from "path";
 
 const storage = multer.diskStorage({
     destination: (req, file, next) => {
-        const path_upload = `uploads/images/${time.getYear()}/${time.getMonth()}`;
-        fs.mkdirSync(path.join("./public", path_upload), { recursive:true });
+        const path_upload = `./public/uploads/images/${time.getYear()}/${time.getMonth()}`;
+        fs.mkdirSync(path_upload, { recursive:true });
         next(null, path_upload);
     },
     filename: (req, file, next) => { 
