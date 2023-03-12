e47e669fa31e09cbefb62d315e57558b8f56b923
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-03-12 21:00:35 +0700

    authN, authZ middleware

--- server/app/routes/auth.route.ts
+++ server/app/routes/auth.route.ts
@@ -1,12 +1,12 @@
 import * as auth from "../controllers/auth.controller";
 import express, { Express } from "express";
-import * as authentication from "../middlewares/authentication";
+import * as authMiddleware from "../middlewares/auth";
 
 export const AuthRoutes = (app: Express) => {
     const router = express.Router();
 
     router.post("/", auth.login);
-    router.get("/", [authentication.verifyToken()], auth.testLogin);
+    router.get("/", [authMiddleware.verifyToken(), authMiddleware.require_admin()], auth.testLogin);
 
     app.use("/api/auth", router);
 }
