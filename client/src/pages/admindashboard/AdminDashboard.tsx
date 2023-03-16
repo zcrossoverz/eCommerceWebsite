4f2fdd95277d49c9a8ee1d1921d04610eecb2093
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-03-16 21:14:46 +0700

    fix active tab admin dashboard

--- client/src/pages/admindashboard/AdminDashboard.tsx
+++ client/src/pages/admindashboard/AdminDashboard.tsx
@@ -1,4 +1,3 @@
-
 import { useSelector } from 'react-redux';
 import SidebarDashboard from 'src/components/admindashboard/sidebardashboard';
 import { dashboardTabInterface, dashboard_tab } from 'src/constants/adminTab';
@@ -12,7 +11,7 @@ function AdminDashboard() {
   return (
     <div className='z-1 flex min-h-screen gap-2 bg-blue-50/50 p-4'>
       <div className='fixed hidden h-[calc(100vh-32px)] w-72 rounded-xl bg-sidebar_dashboard md:block md:overflow-hidden'>
-        <SidebarDashboard active={dashboard_tab[currentTab].name} />
+        <SidebarDashboard />
       </div>
       <div className='-mt-2 w-screen md:ml-80'>{dashboard_tab[currentTab].component}</div>
     </div>
