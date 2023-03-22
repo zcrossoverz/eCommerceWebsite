7653fb54d9e73ddc4bc880ab5345db49b9172bf5
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-03-22 09:50:37 +0700

    change password

--- server/app/routes/user.route.ts
+++ server/app/routes/user.route.ts
@@ -2,6 +2,7 @@
 import * as user from "../controllers/user.controller";
 import express, { Express } from "express";
 import * as validation from "../middlewares/validation";
+import * as auth from "../middlewares/auth";
 
 export const UserRoutes = (app: Express) => {
     const router = express.Router();
@@ -12,6 +13,7 @@ export const UserRoutes = (app: Express) => {
     router.put("/:id", [validation.validateEmail, validation.validatePhoneNumber], user.updateOne); 
     router.post("/:id/add_address", user.addAddress);
     router.patch("/:id/set_default_address", user.setDefaultAddress);
+    router.patch("/change_password", auth.verifyToken(), user.changePassword);
     router.put("/:id_user/update_address/:id_address", user.updateAddress);
     router.delete("/:id", user.deleteOne);
     router.delete("/:id_user/delete_address/:id_address", user.deleteAddress);
