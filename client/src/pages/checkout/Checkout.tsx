040a24695b1e8dfc94310f500b8fb31fc1392264
Nguyen Minh Dang <minhdang26082001@gmail.com>
2023-03-28 12:52:20 +0700

    handle checkout with paypal

--- client/src/pages/checkout/Checkout.tsx
+++ client/src/pages/checkout/Checkout.tsx
@@ -55,12 +55,6 @@ function Checkout() {
     id: -1,
     content: '',
   });
-  // const dispatch = useDispatch();
-  // const throttled = useRef(
-  //   throttle((order: Order) => {
-  //     takeOrderMutation.mutate(order);
-  //   }, Infinity)
-  // );
   const { data: orderItems } = useQuery({
     queryKey: ['id', stateOrderItems.current.id],
     queryFn: () => orderApi.getOneOrder(stateOrderItems.current.id),
@@ -87,6 +81,9 @@ function Checkout() {
   });
   const updateAddressOrderMutation = useMutation({
     mutationFn: (body: { id: number; address: string }) => orderApi.updateAddressOrder(body.address, body.id),
+  });
+  const updateStatusPaymentMutation = useMutation({
+    mutationFn: (id: number) => orderApi.updateStatusPayment(id),
   });
 
   useEffect(() => {
@@ -140,13 +137,12 @@ function Checkout() {
       return;
     }
   };
-  const handleCheckoutOrder = () => {
+  const handleCheckoutOrder = (status?: string) => {
+    console.log(status);
     if (address.content && address.id !== orderItems?.data.user.default_address && orderItems?.data.order_id) {
-      console.log('call address');
       updateAddressOrderMutation.mutate({ id: orderItems.data.order_id, address: address.content });
     }
     if (paymentMethod.method && orderItems?.data.order_id) {
-      console.log('call payment method', paymentMethod.method);
       selectMethodMutation.mutate(
         { id: orderItems.data.order_id, method: paymentMethod.method },
         {
@@ -154,17 +150,33 @@ function Checkout() {
             if (!userInfo?.phone) {
               toast.error('Bạn điền số điện thoại', { autoClose: 1500 });
               return;
-            }
-            if (orderItems?.data.order_id && userInfo?.phone) {
-              updateOrderMutation.mutate(
-                { id: orderItems.data.order_id, status: 'PROCESSING' },
-                {
+            } else {
+              if (status && status === 'COMPLETED') {
+                updateStatusPaymentMutation.mutate(orderItems.data.order_id, {
                   onSuccess: () => {
-                    toast.success('Thanh toán thành công');
-                    navigate(path.myOrders);
+                    updateOrderMutation.mutate(
+                      { id: orderItems.data.order_id, status: 'PROCESSING' },
+                      {
+                        onSuccess: () => {
+                          toast.success('Thanh toán thành công');
+                          navigate(path.myOrders);
+                        },
+                      }
+                    );
                   },
-                }
-              );
+                });
+              }
+              if (!status) {
+                updateOrderMutation.mutate(
+                  { id: orderItems.data.order_id, status: 'PROCESSING' },
+                  {
+                    onSuccess: () => {
+                      toast.success('Thanh toán thành công');
+                      navigate(path.myOrders);
+                    },
+                  }
+                );
+              }
             }
           },
         }
@@ -347,122 +359,126 @@ function Checkout() {
             </div>
           </div>
         </div>
-        <div className='col-span-2 border-l p-2 lg:col-span-1'>
-          <div className='mb-2 flex items-center'>
-            <FaWallet className='text-xl text-blue-400' />
-            <h2 className='ml-2 text-xl font-semibold'>Thanh toán</h2>
-          </div>
-          <div className='w-full'>
-            <div>
-              <h3 className='mb-2 bg-slate-300 p-2'>Chọn phương thức thanh toán</h3>
+        {orderItems?.data.order_id && orderItems.data.status === 'PENDING' ? (
+          <div className='col-span-2 border-l p-2 lg:col-span-1'>
+            <div className='mb-2 flex items-center'>
+              <FaWallet className='text-xl text-blue-400' />
+              <h2 className='ml-2 text-xl font-semibold'>Thanh toán</h2>
+            </div>
+            <div className='w-full'>
               <div>
-                <button
-                  onClick={() => setPaymentMethod({ method: 'CASH_ON_DELIVERY', id: 1 })}
-                  className={classNames(
-                    'flex w-full cursor-pointer items-center rounded-md p-1 duration-200 hover:bg-slate-200',
-                    {
-                      'font-semibold italic text-orange-500': paymentMethod.id === 1,
-                    }
-                  )}
-                >
-                  <span className='flex h-10 w-10 items-center justify-center rounded-md  bg-green-400'>
-                    <HiOutlineHomeModern className='text-3xl text-white' />
-                  </span>
-                  <h4 className='ml-2'>Thanh toán khi nhận hàng</h4>
-                </button>
-                <button
-                  onClick={() => setPaymentMethod({ method: 'PAYPAL', id: 2 })}
-                  className={classNames(
-                    'flex w-full cursor-pointer items-center rounded-md p-1 duration-200 hover:bg-slate-200',
-                    {
-                      'font-semibold italic text-orange-500': paymentMethod.id === 2,
-                    }
-                  )}
-                >
-                  <span className='flex h-10 w-10 items-center justify-center rounded-md  bg-blue-400'>
-                    <BsPaypal className='text-3xl text-white' />
-                  </span>
-                  <h4 className='ml-2'>Thanh toán bằng Paypal</h4>
-                </button>
-              </div>
-            </div>
-            <div className='mt-4'>
-              <h3 className='bg-slate-300 p-2'>Mã giảm giá</h3>
-              <div className='mt-2 flex items-center p-1'>
-                <input
-                  type='text'
-                  id='small-input'
-                  className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-xs'
-                />
-                <button
-                  type='button'
-                  // onClick={() => throttled.current()()}
-                  className='ml-2 whitespace-nowrap rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700'
-                >
-                  Chọn mã
-                </button>
-              </div>
-            </div>
-            <div className='mt-2'>
-              <h3 className='bg-slate-300 p-2'>Thông tin đơn hàng</h3>
-              <div className='mt-2 p-1'>
-                <div className='flex w-full items-center justify-between'>
-                  <span className='text-left text-gray-400'>Tạm tính ({price.totalQuantity} sản phẩm)</span>
-                  <span className='text-end'>{formatPrice(price?.total || 0)}</span>
-                </div>
-                <div className='flex w-full items-center justify-between'>
-                  <span className='text-left text-gray-400'>Giảm giá</span>
-                  <span className='text-end'>{formatPrice(price.discount)}</span>
-                </div>
-                <div className='mt-2 flex w-full items-center justify-between'>
-                  <span className='text-left text-gray-400'>Tổng cộng</span>
-                  <span className='text-end text-lg font-semibold text-orange-400'>
-                    {formatPrice(price?.finalPrice)}
-                  </span>
+                <h3 className='mb-2 bg-slate-300 p-2'>Chọn phương thức thanh toán</h3>
+                <div>
+                  <button
+                    onClick={() => setPaymentMethod({ method: 'CASH_ON_DELIVERY', id: 1 })}
+                    className={classNames(
+                      'flex w-full cursor-pointer items-center rounded-md p-1 duration-200 hover:bg-slate-200',
+                      {
+                        'font-semibold italic text-orange-500': paymentMethod.id === 1,
+                      }
+                    )}
+                  >
+                    <span className='flex h-10 w-10 items-center justify-center rounded-md  bg-green-400'>
+                      <HiOutlineHomeModern className='text-3xl text-white' />
+                    </span>
+                    <h4 className='ml-2'>Thanh toán khi nhận hàng</h4>
+                  </button>
+                  <button
+                    onClick={() => setPaymentMethod({ method: 'PAYPAL', id: 2 })}
+                    className={classNames(
+                      'flex w-full cursor-pointer items-center rounded-md p-1 duration-200 hover:bg-slate-200',
+                      {
+                        'font-semibold italic text-orange-500': paymentMethod.id === 2,
+                      }
+                    )}
+                  >
+                    <span className='flex h-10 w-10 items-center justify-center rounded-md  bg-blue-400'>
+                      <BsPaypal className='text-3xl text-white' />
+                    </span>
+                    <h4 className='ml-2'>Thanh toán bằng Paypal</h4>
+                  </button>
                 </div>
               </div>
-            </div>
-            <div className='mt-2 flex items-center justify-center'>
-              {paymentMethod?.method === 'CASH_ON_DELIVERY' ? (
-                <button
-                  onClick={handleCheckoutOrder}
-                  type='button'
-                  className='mr-2 mb-2 rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800'
-                >
-                  Xác nhận thanh toán
-                </button>
-              ) : (
-                <PayPalScriptProvider
-                  options={{
-                    'client-id': 'AQxm6BTwl0EiN9LhMlyXqgxwOYAWt4Uw3wu7LBDTlxI4MgUdqby0f6-awq5CI-s_xgpISXV1vbpD1kHn',
-                  }}
-                >
-                  <PayPalButtons
-                    createOrder={(data, actions) => {
-                      const price_usd = Number(price.finalPrice) * 0.000043;
-                      return actions.order.create({
-                        purchase_units: [
-                          {
-                            amount: {
-                              value: `${price_usd.toFixed(2)}`,
-                            },
-                          },
-                        ],
-                      });
-                    }}
-                    onApprove={(data, actions) => {
-                      return actions.order
-                        ? actions.order?.capture().then((detail) => {
-                            console.log(detail);
-                          })
-                        : Promise.resolve();
-                    }}
+              <div className='mt-4'>
+                <h3 className='bg-slate-300 p-2'>Mã giảm giá</h3>
+                <div className='mt-2 flex items-center p-1'>
+                  <input
+                    type='text'
+                    id='small-input'
+                    className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-xs'
                   />
-                </PayPalScriptProvider>
-              )}
+                  <button
+                    type='button'
+                    // onClick={() => throttled.current()()}
+                    className='ml-2 whitespace-nowrap rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700'
+                  >
+                    Chọn mã
+                  </button>
+                </div>
+              </div>
+              <div className='mt-2'>
+                <h3 className='bg-slate-300 p-2'>Thông tin đơn hàng</h3>
+                <div className='mt-2 p-1'>
+                  <div className='flex w-full items-center justify-between'>
+                    <span className='text-left text-gray-400'>Tạm tính ({price.totalQuantity} sản phẩm)</span>
+                    <span className='text-end'>{formatPrice(price?.total || 0)}</span>
+                  </div>
+                  <div className='flex w-full items-center justify-between'>
+                    <span className='text-left text-gray-400'>Giảm giá</span>
+                    <span className='text-end'>{formatPrice(price.discount)}</span>
+                  </div>
+                  <div className='mt-2 flex w-full items-center justify-between'>
+                    <span className='text-left text-gray-400'>Tổng cộng</span>
+                    <span className='text-end text-lg font-semibold text-orange-400'>
+                      {formatPrice(price?.finalPrice)}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className='mt-2 flex items-center justify-center'>
+                {paymentMethod?.method === 'CASH_ON_DELIVERY' ? (
+                  <button
+                    onClick={() => handleCheckoutOrder()}
+                    type='button'
+                    className='mr-2 mb-2 rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800'
+                  >
+                    Xác nhận thanh toán
+                  </button>
+                ) : (
+                  <PayPalScriptProvider
+                    options={{
+                      'client-id': 'AQxm6BTwl0EiN9LhMlyXqgxwOYAWt4Uw3wu7LBDTlxI4MgUdqby0f6-awq5CI-s_xgpISXV1vbpD1kHn',
+                    }}
+                  >
+                    <PayPalButtons
+                      createOrder={(data, actions) => {
+                        const price_usd = Number(price.finalPrice) * 0.000043;
+                        return actions.order.create({
+                          purchase_units: [
+                            {
+                              amount: {
+                                value: `${price_usd.toFixed(2)}`,
+                              },
+                            },
+                          ],
+                        });
+                      }}
+                      onApprove={(data, actions) => {
+                        return actions.order
+                          ? actions.order?.capture().then((detail) => {
+                              handleCheckoutOrder(detail.status);
+                            })
+                          : Promise.resolve();
+                      }}
+                    />
+                  </PayPalScriptProvider>
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className='text-lg font-semibold text-orange-400'>Bạn đã thanh toán thành công</div>
+        )}
       </div>
     </div>
   );
