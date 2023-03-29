f9e6c7627794c670a5425227b98c163182d5a619
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-03-29 10:13:42 +0700

    create brand, delete brand, update brand in admin dashboard

--- client/src/slices/modal.slice.ts
+++ client/src/slices/modal.slice.ts
@@ -4,21 +4,33 @@ import { RootState } from 'src/store';
 const initialState = {
   name: 'not set',
   open: false,
+  nameDefault: '',
+  descriptionDefault: '',
+  id: 0,
 };
 
-export const modalSlice = createSlice({
-  name: 'dashboard_modal',
+export const brandModalSlice = createSlice({
+  name: 'dashboard_brand_modal',
   initialState,
   reducers: {
     popup: (state, actions) => {
       state.name = actions.payload.name;
       state.open = !state.open;
+      state.nameDefault = actions.payload.nameDefault;
+      state.descriptionDefault = actions.payload.descriptionDefault;
+      state.id = actions.payload.id;
     },
   },
 });
 
-export const { popup } = modalSlice.actions;
+export const { popup } = brandModalSlice.actions;
 export const selectCurrentModal = (state: RootState) => {
-  return { name: state.modalReducer.name, open: state.modalReducer.open };
+  return {
+    name: state.modalReducer.name,
+    open: state.modalReducer.open,
+    nameDefault: state.modalReducer.nameDefault,
+    descriptionDefault: state.modalReducer.descriptionDefault,
+    id: state.modalReducer.id,
+  };
 };
-export default modalSlice.reducer;
+export default brandModalSlice.reducer;
