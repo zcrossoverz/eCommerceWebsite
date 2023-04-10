0d2d7614ca73423bfd758327e609a913e27f0074
Nguyen Minh Dang <minhdang26082001@gmail.com>
2023-04-10 21:14:25 +0700

    pull code to handle search product

--- client/src/components/admindashboard/inventorydashboard/index.tsx
+++ client/src/components/admindashboard/inventorydashboard/index.tsx
@@ -22,12 +22,49 @@ export default function InventoryDashboard() {
       <div>
         <LineChart inventory />
       </div>
-      <div>
-        <div className='relative mt-10 rounded-lg border border-gray-200 shadow-md'>
-          <span className='rounded-lg bg-cyan-500 px-2 py-4 text-xl font-semibold uppercase text-white shadow-sm md:absolute md:left-1/2 md:-top-8 md:-translate-x-1/2'>
+      <div className='mt-8'>
+        <form className='max-w-[14rem]'>
+          <label htmlFor='default-search' className='sr-only mb-2 text-sm font-medium text-gray-900 '>
+            Search
+          </label>
+          <div className='relative'>
+            <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
+              <svg
+                aria-hidden='true'
+                className='h-5 w-5 text-gray-500 '
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                />
+              </svg>
+            </div>
+            <input
+              type='search'
+              id='default-search'
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 '
+              placeholder='Search'
+              required
+            />
+            <button
+              type='submit'
+              className='absolute bottom-1.5 right-2 rounded-md bg-blue-700 px-2 py-1 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none'
+            >
+              Search
+            </button>
+          </div>
+        </form>
+        <div className='relative mt-1 rounded-lg border border-gray-200 shadow-md'>
+          <span className='rounded-lg bg-cyan-500 px-2 py-4 text-xl font-semibold uppercase text-white shadow-sm md:absolute md:left-1/2 md:-top-10 md:-translate-x-1/2'>
             Tổng sản phẩm trong kho: {data?.data.total}
           </span>
-          <table className='mt-2 w-full text-left text-sm text-gray-500 '>
+          <table className='w-full text-left text-sm text-gray-500 '>
             <thead className='bg-cyan-400 text-xs uppercase text-gray-700'>
               <tr>
                 <th scope='col' className='px-6 py-6'>
