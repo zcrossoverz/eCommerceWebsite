40e595b8a4592eb299ab949e945c824f355ce95c
Nguyen Minh Dang <minhdang26082001@gmail.com>
2023-04-12 10:45:44 +0700

    responsive inventory btn create

--- client/src/components/admindashboard/inventorydashboard/CreateInboundNote.tsx
+++ client/src/components/admindashboard/inventorydashboard/CreateInboundNote.tsx
@@ -180,7 +180,7 @@ function CreateInboundNote() {
             <div className='relative mt-1'>
               <div className='relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left  focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm'>
                 <Combobox.Input
-                  className='w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0'
+                  className='w-full border border-emerald-500 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0'
                   displayValue={(product: ProductData) =>
                     `${product.name} - ${product.ram}/${product.rom} - ${product.color}`
                   }
