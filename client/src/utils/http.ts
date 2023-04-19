ee06b1bbcd780c1f3ffbbe784333f4590323e1e8
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-04-19 08:41:11 +0700

    change config deploy#2

--- client/src/utils/http.ts
+++ client/src/utils/http.ts
@@ -10,7 +10,7 @@ class Http {
   constructor() {
     this.token = getAccessToken();
     this.instance = axios.create({
-      baseURL: process.env.BASE_URL,
+      baseURL: import.meta.env.BASE_URL || 'http://localhost:3000/api',
       timeout: 10000,
       headers: { 'Content-Type': 'application/json' },
     });
